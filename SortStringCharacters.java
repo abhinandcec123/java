c03ca9ce6b16b8be32d4e78d8bdef0a9f0db6a65
abhinandcec123 <chn25mca045@ceconline.edu>
2026-02-22 20:18:35 +0530

    Correct bubble sort logic and variable usage
    Fix errors in bubble sort implementation and variable declarations.

--- SortStringCharacters.java
+++ SortStringCharacters.java
@@ -1,34 +1,31 @@
 import java.util.Scanner;
 
-public class SortStringCharacters{
-	public static void main(String[] args){
-	Scanner sc=new Scanner(System.in);
-	
-	System.out.print("Enter a string:");
-	String input=sc.nextLine();
-	
-	char[] chars=input.toCharArray();
-	
-	for(int i=0;i<chars.length -1;i++){
-	 for(int j=0;j< chars.length-i-1;j++){
-	if(chars[j]> chars[j+1]){
-		chars temp= chars[j];
-		chars[j]=chars[j+1];
-		chars[j+1]=temp;
-		}
-	}
+public class SortStringCharacters {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        char[] chars = input.toCharArray();
+
+        // Bubble sort for characters
+        for (int i = 0; i < chars.length - 1; i++) {
+            for (int j = 0; j < chars.length - i - 1; j++) {
+                if (chars[j] > chars[j + 1]) {
+
+                    char temp = chars[j];   
+                    chars[j] = chars[j + 1];
+                    chars[j + 1] = temp;
+                }
+            }
+        }
+
+        String sortedString = new String(chars);   
+
+        System.out.println("Sorted string in ascending order: " + sortedString);
+
+        sc.close();
+    }
 }
-
-String sortedstring=new String(chars);
-ers.java:16: error: ']' expecteders.java:16: error: ']' expected
-		chars[j]=char[j+1];
-		              ^
-
-		chars[j]=char[j+1];
-		              ^
-
-System.out.println("Sorted string in ascending order:"+ SortedString);
-
-sc.close();
-}
-} 
