af4eb8241080cb2b886e0b1b09176bc1e26e4eb8
abhinandcec123 <chn25mca045@ceconline.edu>
2026-02-22 19:50:56 +0530

    Implement string sorting functionality
    Added functionality to read and sort strings from user input.

--- sort.java
+++ sort.java
@@ -1,9 +1,22 @@
 import java.util.Scanner;
 import java.util.Arrays;
-class sort{
-public static void main(String args[]){
-Scanner sc=new Scanner(System.in);
-System.out.println("Enter the no of strings");
-int n=sc.nextInt();
-System.out.println("Enter the strings");
 
+class sort {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no of strings");
+        int n = sc.nextInt();
+        sc.nextLine();  
+        String arr[] = new String[n];
+        System.out.println("Enter the strings");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextLine();
+        }
+        Arrays.sort(arr);
+        System.out.println("Sorted strings are:");
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+        sc.close();
+    }
+}
