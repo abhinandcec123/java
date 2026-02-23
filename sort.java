aa8b2bbf08e0a9547f7a0453537f5b8b7edcde42
abhinandcec123 <chn25mca045@ceconline.edu>
2026-02-23 13:11:49 +0530

    Fix formatting issue in string input assignment

--- sort.java
+++ sort.java
@@ -10,7 +10,7 @@ class sort {
         String arr[] = new String[n];
         System.out.println("Enter the strings");
         for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextLine();
+            arr[i]= sc.nextLine();
         }
         Arrays.sort(arr);
         System.out.println("Sorted strings are:");
