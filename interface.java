65e6162f5fa887f60ce83fc21f05b53d71a7b98f
abhinandcec123 <chn25mca045@ceconline.edu>
2026-03-16 12:53:00 +0530

    Add Shape interface and Circle/Rectangle classes

---
+++ interface.java
@@ -0,0 +1,37 @@
+interface Shape {
+    double area();
+    double perimeter();
+}
+
+class Circle implements Shape {
+    private double radius;
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+
+    public double perimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
+
+class Rectangle implements Shape {
+    private double length, breadth;
+
+    Rectangle(double length, double breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    public double area() {
+        return length * breadth;
+    }
+
+    public double perimeter() {
+        return 2 * (length + breadth);
+    }
+}
